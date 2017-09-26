8d2fcfd4495b07b5f88e543a39585268216ed89a
alexmorten <alexandrius.martinus@gmx.de>
2017-09-26 16:18:22 +0200

    yet another

--- app.js
+++ app.js
@@ -6,17 +6,35 @@ const port = process.env.PORT || 4001;
 
 
 const app = express();
-app.use(cors());
-// app.use(function(request, response, next) {
-//   response.header("Access-Control-Allow-Origin", "*");
-//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(cors({ origin: '*' }));
+// Settings for CORS
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 const server = app.listen(port);
 
-const io = socketIo(server); // < Interesting!
+const io = socketIo(server,{
+  log: false,
+  agent: false,
+  origins: '*:*',
+  transports: ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'polling']
+}); // < Interesting!
 // io.origins('*:*')
 
 
